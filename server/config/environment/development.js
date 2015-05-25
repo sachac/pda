234a44af1aeaebccbe299bf787c7cd23fe4a2b6e
Sacha Chua <sacha@sachachua.com>
2015-05-25 18:03:11 -0400

    More tweaks to get this to run

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,6 +3,10 @@
 // Development specific configuration
 // ==================================
 module.exports = {
+ ip:       process.env.OPENSHIFT_NODEJS_IP ||
+            process.env.IP ||
+            undefined,
+
   // MongoDB connection options
   mongo: {
     uri: 'mongodb://localhost/pda2-dev'
