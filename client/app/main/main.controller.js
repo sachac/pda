28c437a0f6cb474f3b9a10c0fb514eac3f761c06
Sacha Chua <sacha@sachachua.com>
2015-05-25 16:43:08 -0400

    Display a day-specific routine

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,45 @@
 'use strict';
 
-angular.module('pda2App')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
+function pdaCheckToken($scope, $http, $cookies) {
+  $scope.token = $cookies['QUANTIFIED_TOKEN'];
+  if (!$scope.token) {
+    $http.get('/api/quantified/getToken').success(function(data) {
+      $cookies['QUANTIFIED_TOKEN'] = data.token;
+      $scope.token = data.token;
     });
+  }
+}
 
+function pdaGetActivitySequence() {
+  var workSequence = ['Routines', 'Subway', 'E1 Gen', 'Subway', 'Kitchen', 'Cook', 'Dinner', 'Kitchen', 'Drawing', 'Writing', 'Ni No Kuni', 'Routines', 'Sleep'];
+  var homeSequence = ['Routines', 'Walk Other', 'Gardening', 'Nonfiction', 'Drawing', 'Personal Lunch', 'Ni No Kuni', 'Coding', 'Learn', 'Writing', 'Kitchen', 'Cook', 'Ni No Kuni', 'Routines', 'Sleep'];
+  var weekendSequence = ['Routines', 'Walk Other', 'Gardening', 'Nonfiction', 'Drawing', 'Personal Lunch', 'Ni No Kuni', 'Laundry', 'Tidy', 'Kitchen', 'Cook', 'Ni No Kuni', 'Routines', 'Sleep'];
+  var day = (new Date()).getDay();
+  if (day == 4) {
+    return workSequence;
+  } else if (day == 0 || day == 6) {
+    return weekendSequence;
+  } else {
+    return homeSequence;
+  }                             
+}
+
+angular.module('pda2App')
+  .controller('MainCtrl', function ($scope, $http, $rootScope, $cookies) {
+    $scope.awesomeThings = [];
+    pdaCheckToken($scope, $http, $cookies);
+    $scope.sequenceToday = pdaGetActivitySequence();
+    $scope.sequenceStates = [];
+    $scope.track = function(activity, $index) {
+      $scope.sequenceStates[$index] = 'pending';
+      $http.post('/quantified/time/track.json',
+                 {token: $scope.token, category: activity})
+        .success(function(err, res) {
+          console.log(activity, err, res);
+          $scope.sequenceStates[$index] = 'success';
+          console.log($scope.sequenceStates);
+        }).error(function(err, res) {
+          $scope.sequenceStates[$index] = 'error';
+        });
+    };
   });
--- server/app.js
+++ server/app.js
@@ -3,15 +3,29 @@
  */
 
 'use strict';
+var QUANTIFIED_SERVER = 'http://localhost:3000';
+var proxy = require('express-http-proxy');
 
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var express = require('express');
 var config = require('./config/environment');
+var auth = require('basic-auth-connect');
 // Setup server
 var app = express();
 var server = require('http').createServer(app);
+
+
+app.use(auth(function(user, pass) {
+  return user == 'sacha';
+}));
+        
+app.use('/quantified', proxy(QUANTIFIED_SERVER, { forwardPath: function(req, res) {
+  return require('url').parse(req.url).path;  
+}}));
+
+
 require('./config/express')(app);
 require('./routes')(app);
 
